78a063f18f4c381a329ee37b7dfdd61ef92e6e83
SCAB <camiloab203@gmail.com>
2025-03-01 16:58:29 -0500

    commit2

--- p11/src/main/java/com/example/Main.java
+++ p11/src/main/java/com/example/Main.java
@@ -3,5 +3,8 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int dato = 10;
+        int dato2 = 10;
+        int dato3 = 10;
     }
 }
